7a353c44f1b5671182c222277f5df4d4c2046579
perpetenoah <noah.perpete@student.hepl.be>
2025-02-14 09:32:09 +0100

    Added first iteration example of the loop

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -8,8 +8,24 @@
     <title><?= wp_title('·', false, 'right') . get_bloginfo('name') ?></title>
 </head>
 <body>
-    <h1><?= get_bloginfo('name') ?></h1>
-       <p><?= get_bloginfo('description') ?></p>
-<p></p>
+    <header>
+        <h1><?= get_bloginfo('name') ?></h1>
+        <p><?= get_bloginfo('description') ?></p>
+    </header>
+    <main>
+        <?php
+        // On ouvre "la boucle" (The loop), la structure de contrôle propre à Wordpress:
+        if(have_posts()):while(have_posts()):the_post();?>
+
+            <h2><?= get_the_title(); ?></h2>
+
+            <div><?= get_the_content(); ?></div>
+
+        <?php
+        // On ferme "la boucle" (The loop):
+        endwhile; else:?>
+        <p>La page est vide</p>
+        <?php endif; ?>
+    </main>
 </body>
 </html>
